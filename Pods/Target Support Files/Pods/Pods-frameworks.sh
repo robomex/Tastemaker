4259448e965a5c5adbd0e7f5b2deeb9ab1d46818
robomex <atmorales@gmail.com>
2015-08-25 00:26:16 -0500

    removed buggy PageMenu pod, analytics pods (Google and Mixpanel) i'm not using yet

--- Pods/Target Support Files/Pods/Pods-frameworks.sh
+++ Pods/Target Support Files/Pods/Pods-frameworks.sh
@@ -54,12 +54,8 @@ code_sign_if_enabled() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework 'Pods/JSQMessagesViewController.framework'
   install_framework 'Pods/JSQSystemSoundPlayer.framework'
-  install_framework 'Pods/Mixpanel.framework'
-  install_framework 'Pods/PageMenu.framework'
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework 'Pods/JSQMessagesViewController.framework'
   install_framework 'Pods/JSQSystemSoundPlayer.framework'
-  install_framework 'Pods/Mixpanel.framework'
-  install_framework 'Pods/PageMenu.framework'
 fi
