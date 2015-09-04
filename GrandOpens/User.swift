77d73daf3513d0bddea936608fa66ce48fefd035
robomex <atmorales@gmail.com>
2015-09-03 23:57:46 -0500

    first iteration of the saveVenueVote func

--- GrandOpens/User.swift
+++ GrandOpens/User.swift
@@ -25,3 +25,24 @@ func currentUser() -> User? {
     }
     return nil
 }
+
+func saveVenueVote(venueId: String) {
+    
+    let vote = PFObject(className: "Action")
+    vote.setObject(PFUser.currentUser()!.objectId!, forKey: "byUser")
+    vote.setObject(venueId, forKey: "toObject")
+    vote.setObject("vote", forKey: "type")
+    vote.saveInBackgroundWithBlock(nil)
+    
+    PFQuery(className: "Venue")
+        .getObjectInBackgroundWithId(venueId) {
+            (retrievedVenue: PFObject?, error: NSError?) -> Void in
+            if error != nil {
+                println(error)
+            } else {
+                retrievedVenue?.incrementKey("votes", byAmount: 1)
+            }
+            
+            retrievedVenue?.saveInBackground()
+        }
+}
