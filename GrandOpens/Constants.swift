3306c6623346e08dfef77a9dc27b2d3019045ab1
robomex <atmorales@gmail.com>
2015-09-12 13:49:59 -0500

    add first draft of GOCache, updated cached user attributes constants

--- GrandOpens/Constants.swift
+++ GrandOpens/Constants.swift
@@ -11,30 +11,36 @@ import Foundation
 
 // MARK: User Class
 // Field keys
-let kUserDisplayNameKey                     = "name"
+let kUserDisplayNameKey                         = "name"
 
 
 // MARK: Venue Class
 // Field keys
-let kVenueName                              = "name"
-let kVenueAddress                           = "address"
-let kVenueNeighborhood                      = "neighborhood"
-let kVenueDescription                       = "description"
-let kVenueFoodType                          = "foodType"
+let kVenueName                                  = "name"
+let kVenueAddress                               = "address"
+let kVenueNeighborhood                          = "neighborhood"
+let kVenueDescription                           = "description"
+let kVenueFoodType                              = "foodType"
 
 
 // MARK: Activity Class
 // Field keys
-let kActivityByUserKey                      = "byUser"
-let kActivityToObjectKey                    = "toObject"
-let kActivityType                           = "type"
+let kActivityByUserKey                          = "byUser"
+let kActivityToObjectKey                        = "toObject"
+let kActivityType                               = "type"
 
 // Type values
-let kActivityTypeVote                       = "vote"
+let kActivityTypeVote                           = "vote"
 
 
 // MARK: Cached Venue Attributes
 // keys
-let kVenueAttributesIsVotedByCurrentUserKey = "isVotedByCurrentUser" //the example has a semicolon outside the trailing " - any reason for that?
-let kVenueAttributesVoteCountKey            = "voteCount"
-let kVenueAttributesVotersKey               = "voters"
+let kVenueAttributesIsVotedByCurrentUserKey     = "isVotedByCurrentUser" //the example has a semicolon outside the trailing " - any reason for that?
+let kVenueAttributesVoteCountKey                = "venueVoteCount"
+let kVenueAttributesVotersKey                   = "voters"
+
+
+// MARK: Cached User Attributes
+// keys
+let kUserAttributesVenueVoteCountKey            = "userVenueVoteCount"
+let kUserAttributesIsFollowedByCurrentUserKey   = "isFollowedByCurrentUser"
---
+++ GrandOpens/GOCache.swift
@@ -0,0 +1,129 @@
+//
+//  Cache.swift
+//  GrandOpens
+//
+//  Created by Tony Morales on 9/9/15.
+//  Copyright (c) 2015 Tony Morales. All rights reserved.
+//
+
+import Foundation
+import Parse
+
+final class GOCache {
+    private var cache: NSCache
+    
+    
+    // MARK: Initialization
+    
+    static let sharedCache = GOCache()
+    
+    private init() {
+        self.cache = NSCache()
+    }
+    
+    
+    // MARK: GOCache
+    
+    func clear() {
+        cache.removeAllObjects()
+    }
+    
+    func setAttributesForVenue(venue: PFObject, voters: [PFUser], votedByCurrentUser: Bool) {
+        let attributes = [
+            kVenueAttributesIsVotedByCurrentUserKey: votedByCurrentUser,
+            kVenueAttributesVoteCountKey: voters.count,
+            kVenueAttributesVotersKey: voters
+        ]
+        setAttributes(attributes as! [String: AnyObject], forVenue: venue)
+    }
+    
+    func attributesForVenue(venue: PFObject) -> [String: AnyObject]? {
+        let key: String = self.keyForVenue(venue)
+        return cache.objectForKey(key) as? [String: AnyObject]
+    }
+    
+    func voteCountForVenue(venue: PFObject) -> Int {
+        let attributes: [NSObject: AnyObject]? = self.attributesForVenue(venue)
+        if attributes != nil {
+            return attributes![kVenueAttributesVoteCountKey] as! Int
+        }
+        
+        return 0
+    }
+    
+    func votersForVenue(venue: PFObject) -> [PFUser] {
+        let attributes = attributesForVenue(venue)
+        if attributes != nil {
+            return attributes![kVenueAttributesVotersKey] as! [PFUser]
+        }
+        
+        return [PFUser]()
+    }
+    
+    func setVenueIsVotedByCurrentUser(venue: PFObject, voted: Bool) {
+        var attributes = attributesForVenue(venue)
+        attributes![kVenueAttributesIsVotedByCurrentUserKey] = voted
+        setAttributes(attributes!, forVenue: venue)
+    }
+    
+    func isVenueVotedByCurrentUser(venue: PFObject) -> Bool {
+        let attributes = attributesForVenue(venue)
+        if attributes != nil {
+            return attributes![kVenueAttributesIsVotedByCurrentUserKey] as! Bool
+        }
+        
+        return false
+    }
+    
+    func incrementVoteCountForVenue(venue: PFObject) {
+        let voteCount = voteCountForVenue(venue) + 1
+        var attributes = attributesForVenue(venue)
+        attributes![kVenueAttributesVoteCountKey] = voteCount
+        setAttributes(attributes!, forVenue: venue)
+    }
+    
+    func decrementVoteCountForVenue(venue: PFObject) {
+        let voteCount = voteCountForVenue(venue) - 1
+        if voteCount < 0 {
+            return
+        }
+        var attributes = attributesForVenue(venue)
+        attributes![kVenueAttributesVoteCountKey] = voteCount
+        setAttributes(attributes!, forVenue: venue)
+    }
+    
+    func setAttributesForUser(user: PFUser, venueVoteCount count: Int, followedByCurrentUser following: Bool) {
+        let attributes = [
+            kUserAttributesVenueVoteCountKey: count,
+            kUserAttributesIsFollowedByCurrentUserKey: following
+        ]
+        
+        setAttributes(attributes as! [String: AnyObject], forUser: user)
+    }
+    
+    func attributesForUser(user: PFUser) -> [String: AnyObject]? {
+        let key = keyForUser(user)
+        return cache.objectForKey(key) as? [String: AnyObject]
+    }
+    
+    
+    // MARK: ()
+    
+    func setAttributes(attributes: [String: AnyObject], forVenue venue: PFObject) {
+        let key: String = self.keyForVenue(venue)
+        cache.setObject(attributes, forKey: key)
+    }
+    
+    func setAttributes(attributes: [String: AnyObject], forUser user: PFUser) {
+        let key: String = self.keyForUser(user)
+        cache.setObject(attributes, forKey: key)
+    }
+    
+    func keyForVenue(venue: PFObject) -> String {
+        return "venue_\(venue.objectId)"
+    }
+    
+    func keyForUser(user: PFUser) -> String {
+        return "user_\(user.objectId)"
+    }
+}
