6202f6c4f543f752d4f022b764a914165d7d1c87
robomex <atmorales@gmail.com>
2015-10-23 15:36:08 -0500

    cut unnecessary query and added ZoomToUserLocation and showSimpleAlertWithTitle utilities

--- GrandOpens/GOUtility.swift
+++ GrandOpens/GOUtility.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import Parse
+import MapKit
 
 class GOUtility {
     
@@ -193,13 +194,35 @@ class GOUtility {
         let queryVotes: PFQuery = PFQuery(className: kActivityClassKey)
         queryVotes.whereKey(kActivityToObjectKey, equalTo: venue)
         queryVotes.whereKey(kActivityTypeKey, equalTo: kActivityTypeVote)
+        queryVotes.cachePolicy = cachePolicy
+
+        // No need to use the next two lines since we're only getting results from one query, not two
+//        let query = PFQuery.orQueryWithSubqueries([queryVotes])
+//        query.cachePolicy = cachePolicy
         
-        let query = PFQuery.orQueryWithSubqueries([queryVotes])
-        query.cachePolicy = cachePolicy
 //      "Cannot include ByUser because it is not a pointer to another object, including below two lines (at different points) caused vote icon to not display and vote count
 //        query.includeKey(kActivityByUserKey)
 //        query.includeKey(kActivityToObjectKey)
         
-        return query
+        return queryVotes
+    }
+}
+
+
+// MARK: ()
+
+func showSimpleAlertWithTitle(title: String!, message: String, viewController: UIViewController) {
+    let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+    let action = UIAlertAction(title: "OK", style: .Cancel, handler: nil)
+    alert.addAction(action)
+    viewController.presentViewController(alert, animated: true, completion: nil)
+}
+
+// Below may be useful after adding MapKit
+
+func zoomToUserLocationInMapView(mapView: MKMapView) {
+    if let coordinate = mapView.userLocation.location?.coordinate {
+        let region = MKCoordinateRegionMakeWithDistance(coordinate, 10000, 10000)
+        mapView.setRegion(region, animated: true)
     }
 }
