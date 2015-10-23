379d0d21ced2677ce9ac2889b20ba03408de5cfb
robomex <atmorales@gmail.com>
2015-10-23 15:38:22 -0500

    added VoteDisabled art and button UIControlState.Disabled

--- GrandOpens/GOVenueCellView.swift
+++ GrandOpens/GOVenueCellView.swift
@@ -71,9 +71,10 @@ class GOVenueCellView: PFTableViewCell {
             self.voteButton!.titleLabel!.minimumScaleFactor = 0.8
             self.voteButton!.titleLabel!.adjustsFontSizeToFitWidth = true
             self.voteButton!.adjustsImageWhenHighlighted = false
-            self.voteButton!.adjustsImageWhenDisabled = false
             self.voteButton!.setBackgroundImage(UIImage(named: "VoteNormal.png"), forState: UIControlState.Normal)
             self.voteButton!.setBackgroundImage(UIImage(named: "VoteSelected.png"), forState: UIControlState.Selected)
+            self.voteButton!.setBackgroundImage(UIImage(named: "VoteDisabled.png"), forState: UIControlState.Disabled)
+            
             self.voteButton!.selected = false
         }
         
