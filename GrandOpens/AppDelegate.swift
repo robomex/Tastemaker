a5cc7cca4707da356b0c3e3f205b8057354bd7fc
robomex <atmorales@gmail.com>
2016-01-16 14:57:27 -0600

    fix basic ReachabilitySwift formatting, remove references to location/notification permissioning from AppDelegate, remove CoreData code

--- GrandOpens/AppDelegate.swift
+++ GrandOpens/AppDelegate.swift
@@ -12,7 +12,8 @@ import Parse
 import Bolts
 import MBProgressHUD
 import CoreLocation
-import Onboard
+import ReachabilitySwift
+import Whisper
 
 
 @UIApplicationMain
@@ -45,9 +46,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UITabBarControllerDelegat
         PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
 
         // Use Reachability to monitor connectivity
-        
-        // THIS SHIT IS CRASHING - fix this shit later
-        //self.monitorReachability()
+        self.monitorReachability()
         
         self.initialViewController = storyboard.instantiateViewControllerWithIdentifier("InitialViewController") as? InitialViewController
         
@@ -59,13 +58,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UITabBarControllerDelegat
         
         // CoreLocation shit for region monitoring
         locationManager.delegate = self
-//        locationManager.requestAlwaysAuthorization()
         
         self.window!.rootViewController = self.navController
         self.window!.makeKeyAndVisible()
         
-//        let settings = UIUserNotificationSettings(forTypes: [.Alert, .Badge, .Sound], categories: nil)
-//        application.registerUserNotificationSettings(settings)
         UIApplication.sharedApplication().cancelAllLocalNotifications()
         
         return true
@@ -91,8 +87,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UITabBarControllerDelegat
 
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-        // Saves changes in the application's managed object context before the application terminates.
-        self.saveContext()
     }
     
     
@@ -178,14 +172,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UITabBarControllerDelegat
     }
     
     func monitorReachability() {
-        guard let reachability = Reachability(hostname: "api.parse.com") else {
+        
+        let reachability: Reachability
+        do {
+            reachability = try Reachability.reachabilityForInternetConnection()
+        } catch {
+            print("Unable to create Reachability")
             return
         }
         
         reachability.whenReachable = { (reach: Reachability) in
             self.networkStatus = reach.currentReachabilityStatus
             if self.isParseReachable() && PFUser.currentUser() != nil && self.feedTableViewController!.objects!.count == 0 {
-                // Refresh home on network restoration. Takes care of freshly installed app that failed to loan the home list under bad network conditions.
+                // Refresh home on network restoration. Takes care of freshly installed app that failed to load the home list under bad network conditions.
                 // In this case, they'd see an empty list placeholder (once I add one) and have no way of refreshing the list
                 self.feedTableViewController!.loadObjects()
             }
@@ -193,9 +192,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UITabBarControllerDelegat
         
         reachability.whenUnreachable = { (reach: Reachability) in
             self.networkStatus = reach.currentReachabilityStatus
+            let announcement = Announcement(title: "Internet connect lost!", subtitle: "Shit! You ain't connected!", image: nil, duration: 5.0, action: nil)
+            Shout(announcement, to: self)
         }
         
-        reachability.startNotifier()
+        do {
+            try reachability.startNotifier()
+        } catch {
+            print("Unable to start notifier")
+        }
     }
     
     
@@ -293,82 +298,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UITabBarControllerDelegat
 //            handleRegionEvent(region)
 //        }
 //    }
-    
-    
-    
-
-    // MARK: - Core Data stack
-
-    lazy var applicationDocumentsDirectory: NSURL = {
-        // The directory the application uses to store the Core Data store file. This code uses a directory named "com.GrandOpens.GrandOpens" in the application's documents Application Support directory.
-        let urls = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
-        return urls[urls.count-1] 
-    }()
-
-    lazy var managedObjectModel: NSManagedObjectModel = {
-        // The managed object model for the application. This property is not optional. It is a fatal error for the application not to be able to find and load its model.
-        let modelURL = NSBundle.mainBundle().URLForResource("GrandOpens", withExtension: "momd")!
-        return NSManagedObjectModel(contentsOfURL: modelURL)!
-    }()
-
-    lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator? = {
-        // The persistent store coordinator for the application. This implementation creates and return a coordinator, having added the store for the application to it. This property is optional since there are legitimate error conditions that could cause the creation of the store to fail.
-        // Create the coordinator and store
-        var coordinator: NSPersistentStoreCoordinator? = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
-        let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent("GrandOpens.sqlite")
-        var error: NSError? = nil
-        var failureReason = "There was an error creating or loading the application's saved data."
-        do {
-            try coordinator!.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: nil)
-        } catch var error1 as NSError {
-            error = error1
-            coordinator = nil
-            // Report any error we got.
-            var dict = [String: AnyObject]()
-            dict[NSLocalizedDescriptionKey] = "Failed to initialize the application's saved data"
-            dict[NSLocalizedFailureReasonErrorKey] = failureReason
-            dict[NSUnderlyingErrorKey] = error
-            error = NSError(domain: "YOUR_ERROR_DOMAIN", code: 9999, userInfo: dict)
-            // Replace this with code to handle the error appropriately.
-            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            NSLog("Unresolved error \(error), \(error!.userInfo)")
-            abort()
-        } catch {
-            fatalError()
-        }
-        
-        return coordinator
-    }()
-
-    lazy var managedObjectContext: NSManagedObjectContext? = {
-        // Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.) This property is optional since there are legitimate error conditions that could cause the creation of the context to fail.
-        let coordinator = self.persistentStoreCoordinator
-        if coordinator == nil {
-            return nil
-        }
-        var managedObjectContext = NSManagedObjectContext()
-        managedObjectContext.persistentStoreCoordinator = coordinator
-        return managedObjectContext
-    }()
-
-    // MARK: - Core Data Saving support
-
-    func saveContext () {
-        if let moc = self.managedObjectContext {
-            var error: NSError? = nil
-            if moc.hasChanges {
-                do {
-                    try moc.save()
-                } catch let error1 as NSError {
-                    error = error1
-                    // Replace this implementation with code to handle the error appropriately.
-                    // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                    NSLog("Unresolved error \(error), \(error!.userInfo)")
-                    abort()
-                }
-            }
-        }
-    }
-
 }
 
