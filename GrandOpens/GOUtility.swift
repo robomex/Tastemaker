af906ad51021e58569bcdb243cb884cca2323c3b
robomex <atmorales@gmail.com>
2016-02-13 15:15:57 -0600

    add delay utility function

--- GrandOpens/GOUtility.swift
+++ GrandOpens/GOUtility.swift
@@ -309,6 +309,16 @@ func localDateFormatter() -> NSDateFormatter {
     return localDateFormatter
 }
 
+func delay(delay: Double, closure: () -> ()) {
+    dispatch_after(
+        dispatch_time(
+            DISPATCH_TIME_NOW,
+            Int64(delay * Double(NSEC_PER_SEC))
+        ),
+        dispatch_get_main_queue(), closure
+    )
+}
+
 // Below may be useful after adding MapKit
 
 func zoomToUserLocationInMapView(mapView: MKMapView) {
