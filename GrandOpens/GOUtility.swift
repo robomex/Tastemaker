6f45b092475b36db491d8ca6d34d93961169123e
robomex <atmorales@gmail.com>
2016-02-14 09:39:30 -0600

    NSDate extension: time from date calculations

--- GrandOpens/GOUtility.swift
+++ GrandOpens/GOUtility.swift
@@ -319,6 +319,48 @@ func delay(delay: Double, closure: () -> ()) {
     )
 }
 
+// May want to use nscalendar.currentCalendar(). below - I did not initially to prevent changes to/from local time
+extension NSDate {
+    func yearsFrom(date: NSDate) -> Int {
+        return NSCalendar.currentCalendar().components(NSCalendarUnit.Year, fromDate: date, toDate: self, options: []).year
+    }
+    
+    func monthsFrom(date: NSDate) -> Int {
+        return NSCalendar.currentCalendar().components(NSCalendarUnit.Month, fromDate: date, toDate: self, options: []).month
+    }
+    
+    func weeksFrom(date: NSDate) -> Int {
+        return NSCalendar.currentCalendar().components(NSCalendarUnit.WeekOfYear, fromDate: date, toDate: self, options: []).weekOfYear
+    }
+    
+    func daysFrom(date: NSDate) -> Int {
+        return NSCalendar.currentCalendar().components(NSCalendarUnit.Day, fromDate: date, toDate: self, options: []).day
+    }
+    
+    func hoursFrom(date: NSDate) -> Int {
+        return NSCalendar.currentCalendar().components(NSCalendarUnit.Hour, fromDate: date, toDate: self, options: []).hour
+    }
+    
+    func minutesFrom(date: NSDate) -> Int {
+        return NSCalendar.currentCalendar().components(NSCalendarUnit.Minute, fromDate: date, toDate: self, options: []).minute
+    }
+    
+    func secondsFrom(date: NSDate) -> Int {
+        return NSCalendar.currentCalendar().components(NSCalendarUnit.Second, fromDate: date, toDate: self, options: []).second
+    }
+    
+    func offsetFrom(date: NSDate) -> String {
+        if yearsFrom(date)  > 0 { return "\(yearsFrom(date))y"  }
+        if monthsFrom(date) > 0 { return "\(monthsFrom(date))M" }
+        if weeksFrom(date)  > 0 { return "\(weeksFrom(date))w"  }
+        if daysFrom(date)   > 0 { return "\(daysFrom(date))d"   }
+        if hoursFrom(date)  > 0 { return "\(hoursFrom(date))h"  }
+        if minutesFrom(date) > 0 { return "\(minutesFrom(date))m"}
+        if secondsFrom(date) > 0 { return "\(secondsFrom(date))s"}
+        return ""
+    }
+}
+
 // Below may be useful after adding MapKit
 
 func zoomToUserLocationInMapView(mapView: MKMapView) {
