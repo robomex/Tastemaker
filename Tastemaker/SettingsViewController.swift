bd64ab70f395e73640efc432a8702f5c76182813
robomex <atmorales@gmail.com>
2016-05-13 01:33:55 -0500

    closes #33 and closes #34 - brings users opted out of location or notifications from settings tab to settings app to enable

--- Tastemaker/SettingsViewController.swift
+++ Tastemaker/SettingsViewController.swift
@@ -11,6 +11,7 @@ import SafariServices
 import SCLAlertView_Objective_C
 import Firebase
 import Amplitude_iOS
+import PermissionScope
 
 class SettingsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, SFSafariViewControllerDelegate {
     
@@ -25,10 +26,13 @@ class SettingsViewController: UIViewController, UITableViewDataSource, UITableVi
     private var newPassword: String?
     private var newPasswordReentry: String?
     private var email: String?
+    private var needToFixPermissions: Bool?
+    let pscope = PermissionScope()
     
-    var settingsHeadings = ["My Account", "Additional Information", ""]
+    var settingsHeadings = ["My Account", "Permissions", "Additional Information", ""]
     var myAccountRows = ["Nickname", "Muted Users", "Notification Period", "Change Password"]
     var additionalInformationRows = ["Privacy Policy", "Terms of Service"]
+    var fixPermissionsRows = ["Fix Permissions"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -45,6 +49,17 @@ class SettingsViewController: UIViewController, UITableViewDataSource, UITableVi
         edgesForExtendedLayout = UIRectEdge.All
         settingsTableView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: tabBarHeight!, right: 0)
         self.view.addSubview(settingsTableView)
+        
+        pscope.addPermission(NotificationsPermission(notificationCategories: nil), message: "Allow to know when someone replies to you + be the first to hear news")
+        pscope.addPermission(LocationAlwaysPermission(), message: "Enable to find the newest places around + unlock voting for places you've been")
+        pscope.headerLabel.text = "Let's get set up"
+        pscope.bodyLabel.text = "Tastemaker works best with the following permissions"
+        pscope.permissionButtonBorderColor = kBlue
+        pscope.permissionButtonTextColor = kBlue
+        pscope.closeButtonTextColor = UIColor.clearColor()
+        pscope.authorizedButtonColor = kBlue
+        pscope.unauthorizedButtonColor = kRed
+        pscope.closeButton.titleLabel?.text = "X"
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -76,6 +91,10 @@ class SettingsViewController: UIViewController, UITableViewDataSource, UITableVi
                 self.settingsTableView.reloadData()
             }
         })
+        
+        if PermissionScope().statusLocationAlways() == .Unauthorized || PermissionScope().statusNotifications() == .Unauthorized {
+            needToFixPermissions = true
+        }
         
         let tracker = GAI.sharedInstance().defaultTracker
         tracker.set(kGAIScreenName, value: "SettingsViewController")
@@ -110,16 +129,34 @@ class SettingsViewController: UIViewController, UITableViewDataSource, UITableVi
                 return 3
             }
         case 1:
-            return 2
+            if (needToFixPermissions != nil) && needToFixPermissions! {
+                return 1
+            } else {
+                return 2
+            }
         case 2:
-            return 1
+            if (needToFixPermissions != nil) && needToFixPermissions! {
+                return 2
+            } else {
+                return 1
+            }
+        case 3:
+            if (needToFixPermissions != nil) && needToFixPermissions! {
+                return 1
+            } else {
+                return 0
+            }
         default:
             return 0
         }
     }
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 3
+        if (needToFixPermissions != nil) && needToFixPermissions! {
+            return 4
+        } else {
+            return 3
+        }
     }
 
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
@@ -152,10 +189,24 @@ class SettingsViewController: UIViewController, UITableViewDataSource, UITableVi
                 cell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
             }
         case 1:
-            cell.textLabel?.text = additionalInformationRows[indexPath.row]
             cell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
+            if (needToFixPermissions != nil) && needToFixPermissions! {
+                cell.textLabel?.text = fixPermissionsRows[indexPath.row]
+                cell.textLabel?.textColor = kRed
+            } else {
+                cell.textLabel?.text = additionalInformationRows[indexPath.row]
+            }
         case 2:
-            cell.textLabel?.text = "Log Out"
+            if (needToFixPermissions != nil) && needToFixPermissions! {
+                cell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
+                cell.textLabel?.text = additionalInformationRows[indexPath.row]
+            } else {
+                cell.textLabel?.text = "Log Out"
+            }
+        case 3:
+            if (needToFixPermissions != nil) && needToFixPermissions! {
+                cell.textLabel?.text = "Log Out"
+            }
         default:
             cell.textLabel?.text = ""
         }
@@ -163,7 +214,20 @@ class SettingsViewController: UIViewController, UITableViewDataSource, UITableVi
     }
 
     func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return settingsHeadings[section]
+        if (needToFixPermissions != nil) && needToFixPermissions! {
+            return settingsHeadings[section]
+        } else {
+            switch section {
+            case 0:
+                return settingsHeadings[0]
+            case 1:
+                return settingsHeadings[2]
+            case 2:
+                return settingsHeadings[3]
+            default:
+                return ""
+            }
+        }
     }
     
     func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
@@ -327,23 +391,97 @@ class SettingsViewController: UIViewController, UITableViewDataSource, UITableVi
                 changePasswordAlert.showEdit(view.window?.rootViewController, title: nil, subTitle: "Enter your old password and choose a new password 6+ characters long ", closeButtonTitle: "Cancel", duration: 0)
             }
         } else if indexPath.section == 1 {
-            switch (indexPath.row) {
-            case 0:
-                let safariVC = SFSafariViewController(URL: NSURL(string: kPrivacyPolicyURL)!)
-                safariVC.delegate = self
-                self.presentViewController(safariVC, animated: true, completion: nil)
+            if (needToFixPermissions != nil) && needToFixPermissions! {
                 
-                Amplitude.instance().logEvent("Viewed Privacy", withEventProperties: ["Viewed From": "Settings"])
-            case 1:
-                let safariVC = SFSafariViewController(URL: NSURL(string: kTermsOfServiceURL)!)
-                safariVC.delegate = self
-                self.presentViewController(safariVC, animated: true, completion: nil)
+                Amplitude.instance().logEvent("Viewed Fix Permissions")
                 
-                Amplitude.instance().logEvent("Viewed Terms", withEventProperties: ["Viewed From": "Settings"])
-            default:
-                return
+                pscope.show({
+                    finished, results in
+                    
+                    if results[0].status != .Unknown && results[1].status != .Unknown {
+                        
+                        if results[0].status == .Authorized {
+                            UIApplication.sharedApplication().registerForRemoteNotifications()
+                            
+                            Amplitude.instance().logEvent("Subsequent Notification Permission", withEventProperties: ["Status": "Authorized"])
+                            Amplitude.instance().identify(AMPIdentify().set("Notification Permission", value: "Authorized"))
+                        } else if results[0].status == .Unauthorized {
+                            Amplitude.instance().logEvent("Subsequent Notification Permission", withEventProperties: ["Status": "Unauthorized"])
+                            Amplitude.instance().identify(AMPIdentify().set("Notification Permission", value: "Unauthorized"))
+                        } else if results[0].status == .Disabled {
+                            Amplitude.instance().logEvent("Subsequent Notification Permission", withEventProperties: ["Status": "Disabled"])
+                            Amplitude.instance().identify(AMPIdentify().set("Notification Permission", value: "Disabled"))
+                        }
+                        
+                        if results[1].status == .Authorized {
+                            Amplitude.instance().logEvent("Subsequent Location Permission", withEventProperties: ["Status": "Authorized"])
+                            Amplitude.instance().identify(AMPIdentify().set("Location Permission", value: "Authorized"))
+                        } else if results[1].status == .Unauthorized {
+                            Amplitude.instance().logEvent("Subsequent Location Permission", withEventProperties: ["Status": "Unauthorized"])
+                            Amplitude.instance().identify(AMPIdentify().set("Location Permission", value: "Unauthorized"))
+                        } else if results[1].status == .Disabled {
+                            Amplitude.instance().logEvent("Subsequent Location Permission", withEventProperties: ["Status": "Disabled"])
+                            Amplitude.instance().identify(AMPIdentify().set("Location Permission", value: "Disabled"))
+                        }
+                        
+                        self.pscope.hide()
+                        if PermissionScope().statusLocationAlways() != .Unauthorized && PermissionScope().statusNotifications() != .Unauthorized {
+                            self.needToFixPermissions = false
+                        }
+                        self.settingsTableView.reloadData()
+                    }
+                    }, cancelled: {
+                        results in
+
+                })
+            } else {
+                switch (indexPath.row) {
+                case 0:
+                    let safariVC = SFSafariViewController(URL: NSURL(string: kPrivacyPolicyURL)!)
+                    safariVC.delegate = self
+                    self.presentViewController(safariVC, animated: true, completion: nil)
+                    
+                    Amplitude.instance().logEvent("Viewed Privacy", withEventProperties: ["Viewed From": "Settings"])
+                case 1:
+                    let safariVC = SFSafariViewController(URL: NSURL(string: kTermsOfServiceURL)!)
+                    safariVC.delegate = self
+                    self.presentViewController(safariVC, animated: true, completion: nil)
+                    
+                    Amplitude.instance().logEvent("Viewed Terms", withEventProperties: ["Viewed From": "Settings"])
+                default:
+                    return
+                }
             }
-        } else if indexPath.section == 2 && indexPath.row == 0 {
+        } else if indexPath.section == 2 {
+            if (needToFixPermissions != nil) && needToFixPermissions! {
+                switch (indexPath.row) {
+                case 0:
+                    let safariVC = SFSafariViewController(URL: NSURL(string: kPrivacyPolicyURL)!)
+                    safariVC.delegate = self
+                    self.presentViewController(safariVC, animated: true, completion: nil)
+                    
+                    Amplitude.instance().logEvent("Viewed Privacy", withEventProperties: ["Viewed From": "Settings"])
+                case 1:
+                    let safariVC = SFSafariViewController(URL: NSURL(string: kTermsOfServiceURL)!)
+                    safariVC.delegate = self
+                    self.presentViewController(safariVC, animated: true, completion: nil)
+                    
+                    Amplitude.instance().logEvent("Viewed Terms", withEventProperties: ["Viewed From": "Settings"])
+                default:
+                    return
+                }
+            } else {
+                let alertController = UIAlertController(title: "Are you sure?", message: nil, preferredStyle: .Alert)
+                alertController.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: { (action: UIAlertAction!) in
+                    print("Cancel")
+                }))
+                alertController.addAction(UIAlertAction(title: "Log Out", style: UIAlertActionStyle.Default, handler: { (action: UIAlertAction!) in
+                    Amplitude.instance().logEvent("Logged Out")
+                    (UIApplication.sharedApplication().delegate as! AppDelegate).logOut()
+                }))
+                UIApplication.sharedApplication().keyWindow?.rootViewController?.presentViewController(alertController, animated: true, completion: nil)
+            }
+        } else if indexPath.section == 3 {
             let alertController = UIAlertController(title: "Are you sure?", message: nil, preferredStyle: .Alert)
             alertController.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: { (action: UIAlertAction!) in
                 print("Cancel")
